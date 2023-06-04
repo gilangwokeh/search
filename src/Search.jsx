f46c01c6121b62b9731c446c38fa4a715542939f
gilangwokeh <gilangwokeh12@gmail.com>
2023-06-04 13:36:50 +0700

    create

---
+++ src/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <section className='py-4 container'>
+      <div className='row justify-content-center'>
+         <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
+          <div className='card p-0 overflow-hidden h-100 shadow'>
+            <img src="" alt=""  className='card-img-top'/>
+            <div className='card-body'>
+                <h5 className='card-title'>card title</h5>
+                <p className='card-text'>card desc</p>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </section>
+  )
+}
+
+export default Search
