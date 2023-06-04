7c65f193aec93878740004b3009e4aed76b76009
gilangwokeh <gilangwokeh12@gmail.com>
2023-06-04 20:59:15 +0700

    update

---
+++ src/Data.jsx
@@ -0,0 +1,93 @@
+import  img1 from './img/gambar1.jpeg';
+import  img2 from './img/gambar2.jpeg';
+import  img3 from './img/gambar4.jpeg';
+import  img4 from './img/gambar5.jpeg';
+
+const data = {
+    cardData: [
+        {
+            id:1,
+            img: img1,
+            title: 'blog 1',
+            desc: 'desc 1',
+        },
+        {
+            id:2,
+            img: img2,
+            title: 'blog 2',
+            desc: 'desc 2',
+        },
+        {
+            id:3,
+            img: img3,
+            title: 'blog 2',
+            desc: 'desc 2',
+        },
+        {
+            id:4,
+            img: img4,
+            title: 'blog 4',
+            desc: 'desc 4',
+        }
+        ,
+        {
+            id:5,
+            img: img4,
+            title: 'blog 5',
+            desc: 'desc 5',
+        },
+        {
+            id:6,
+            img: img1,
+            title: 'blog 1',
+            desc: 'desc 1',
+        }
+        ,
+        {
+            id:7,
+            img: img2,
+            title: 'blog 2',
+            desc: 'desc 2',
+        }
+        ,
+        {
+            id:8,
+            img: img3,
+            title: 'blog 3',
+            desc: 'desc 3',
+        },
+        {
+            id:9,
+            img: img4,
+            title: 'blog 4',
+            desc: 'desc 4',
+        },
+        {
+            id:10,
+            img: img1,
+            title: 'blog 1',
+            desc: 'desc 1',
+        },
+        {
+            id:11,
+            img: img2,
+            title: 'blog 2',
+            desc: 'desc 2',
+        },
+        {
+            id:12,
+            img: img3,
+            title: 'blog 3',
+            desc: 'desc 3',
+        },
+        {
+            id:13,
+            img: img4,
+            title: 'blog 4',
+            desc: 'desc 4',
+        }
+        
+    ]
+}
+
+export default data;
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import data from './Data'
 const Search = () => {
+  const [Filter, setFilter] = useState('');
+  const  searchText = (event) => {
+      setFilter(event.target.value);
+  }
+
+  let dataSearch = data.cardData.filter(item => {
+    return Object.keys(item).some(key => 
+      item[key].toString().toLowerCase().includes(Filter.toString().toLowerCase())
+      )
+  });
   return (
     <section className='py-4 container'>
       <div className='row justify-content-center'>
-         <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
-          <div className='card p-0 overflow-hidden h-100 shadow'>
-            <img src="" alt=""  className='card-img-top'/>
-            <div className='card-body'>
-                <h5 className='card-title'>card title</h5>
-                <p className='card-text'>card desc</p>
-            </div>
+        <div className='col-12 mb-5'>
+          <div className='mb-2 col-4 mx-auto text-center'>
+              <label className='form-lable h4'>Search</label>
+              <input type="text" className='from-control' value={Filter} onChange={searchText.bind(this)}/>
           </div>
         </div> 
+        {dataSearch.map((item,index)=> {
+          return(
+            <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
+            <div className='card p-0 overflow-hidden h-100 shadow'>
+              <img src={item.img} alt=""  className='card-img-top'/>
+              <div className='card-body'>
+                  <h5 className='card-title'>{item.title}</h5>
+                  <p className='card-text'>{item.desc}</p>
+              </div>
+            </div>
+          </div> 
+          )
+        })}
       </div>
     </section>
   )
